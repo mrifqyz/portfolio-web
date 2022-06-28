f36c4252164db9de5b09a4ebbd64d25c5aa4a530
Rifqy Zulkarnaen <mrifqyz@Rifqys-MacBook-Air.local>
2022-06-29 00:30:39 +0700

    POR-3 implement landing page

--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-landing-page',
@@ -6,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./landing-page.component.scss']
 })
 export class LandingPageComponent implements OnInit {
+  imagePath = environment.imagePath;
 
   constructor() { }
 

c3df368493fe37656ccb80c9557203a43423e4de
Rifqy Zulkarnaen <mrifqyz@Rifqys-MacBook-Air.local>
2022-06-28 16:46:58 +0700

    Generate angular component for feature

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import { AboutPageComponent } from './about-page/about-page.component';
+import { NotFoundPageComponent } from './not-found-page/not-found-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingPageComponent,
+    AboutPageComponent,
+    NotFoundPageComponent
   ],
   imports: [
     BrowserModule,

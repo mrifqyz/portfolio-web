91b5404e13171bc446a1626d9c8791d1d9c46f85
Rifqy Zulkarnaen <mrifqyz@Rifqys-MacBook-Air.local>
2022-06-29 01:21:07 +0700

    POR-4 404 page implementation

--- src/app/not-found-page/not-found-page.component.ts
+++ src/app/not-found-page/not-found-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-not-found-page',
@@ -6,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./not-found-page.component.scss']
 })
 export class NotFoundPageComponent implements OnInit {
+  imagePath = environment.imagePath;
 
   constructor() { }
 
